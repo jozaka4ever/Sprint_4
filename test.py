d049ac8e7648dc09dbfafc6b829491a06281b009
jozaka4ever <163282584+jozaka4ever@users.noreply.github.com>
2026-07-03 15:35:08 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,66 +1,100 @@
 import pytest
-from main import BooksCollector
-
-@pytest.fixture
-def collector():
-    return BooksCollector()
 
 class TestBooksCollector:
-    def test_add_new_book_book_added(self, collector):
-        collector.add_new_book('Война и Мир')
-        assert 'Война и Мир' in collector.get_books_genre()
 
-    def test_set_book_genre_genre_setted(self, collector):
+    @pytest.mark.parametrize('book_name', [
+        'А',
+        'А' * 40
+    ])
+    def test_add_new_book_valid_name_length_book_added(self, collector, book_name):
+        collector.add_new_book(book_name)
+
+        assert book_name in collector.get_books_genre()
+
+    @pytest.mark.parametrize('book_name', [
+        '',
+        'А' * 41
+    ])
+    def test_add_new_book_invalid_name_length_book_not_added(self, collector, book_name):
+        collector.add_new_book(book_name)
+
+        assert book_name not in collector.get_books_genre()
+
+    def test_set_book_genre_existing_book_and_valid_genre_genre_set(self, collector):
         collector.add_new_book('Война и Мир')
         collector.set_book_genre('Война и Мир', 'Фантастика')
-        assert collector.get_books_genre() == {'Война и Мир':'Фантастика'}
 
-    def test_get_books_genre_genre_shows(self, collector):
+        assert collector.get_book_genre('Война и Мир') == 'Фантастика'
+
+    def test_get_book_genre_existing_book_returns_genre(self, collector):
         collector.add_new_book('Ведьмак')
         collector.set_book_genre('Ведьмак', 'Фантастика')
+
         assert collector.get_book_genre('Ведьмак') == 'Фантастика'
 
-    @pytest.mark.parametrize('book_name, genre_name',[
-    ['Ведьмак', 'Фантастика'],
-    ['Шерлок Холмс', 'Детективы'],
-    ['Властелин колец', 'Фантастика']
-])
-    def test_get_books_with_specific_genre_shows_books(self, collector, book_name, genre_name):
-        collector.add_new_book(book_name)
-        collector.set_book_genre(book_name, genre_name)
-        assert book_name in collector.get_books_with_specific_genre(genre_name)
+    def test_get_books_with_specific_genre_returns_books_with_selected_genre(self, collector):
+        collector.add_new_book('Ведьмак')
+        collector.add_new_book('Шерлок Холмс')
+        collector.set_book_genre('Ведьмак', 'Фантастика')
+        collector.set_book_genre('Шерлок Холмс', 'Детективы')
 
-    @pytest.mark.parametrize('book_name, genre_name, is_for_children',[
-    ['Ведьмак', 'Фантастика', True],
-    ['Шерлок Холмс', 'Детективы', False],
-    ['Властелин колец', 'Фантастика', True],
-    ['Оно', 'Ужасы', False]
-])
-    def test_get_books_for_children_allowed_genres_added(self, collector, book_name, genre_name, is_for_children):
-        collector.add_new_book(book_name)
-        collector.set_book_genre(book_name, genre_name)
-        assert (book_name in collector.get_books_for_children()) == is_for_children
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Ведьмак']
 
-    def test_add_book_in_favorites_book_added(self, collector):
+    def test_get_books_genre_returns_books_genre_dictionary(self, collector):
         collector.add_new_book('Ведьмак')
         collector.set_book_genre('Ведьмак', 'Фантастика')
+
+        assert collector.get_books_genre() == {'Ведьмак': 'Фантастика'}
+
+    @pytest.mark.parametrize('book_name, genre_name', [
+        ['Ведьмак', 'Фантастика'],
+        ['Властелин колец', 'Комедии'],
+        ['Кот в сапогах', 'Мультфильмы']
+    ])
+    def test_get_books_for_children_book_with_allowed_genre_is_returned(
+            self, collector, book_name, genre_name
+    ):
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre_name)
+
+        assert book_name in collector.get_books_for_children()
+
+    @pytest.mark.parametrize('book_name, genre_name', [
+        ['Шерлок Холмс', 'Детективы'],
+        ['Оно', 'Ужасы']
+    ])
+    def test_get_books_for_children_book_with_age_rating_genre_is_not_returned(
+            self, collector, book_name, genre_name
+    ):
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre_name)
+
+        assert book_name not in collector.get_books_for_children()
+
+    def test_add_book_in_favorites_existing_book_book_added(self, collector):
+        collector.add_new_book('Ведьмак')
         collector.add_book_in_favorites('Ведьмак')
+
         assert 'Ведьмак' in collector.get_list_of_favorites_books()
 
-    def test_delete_book_from_favorites_book_deleted(self, collector):
+    def test_delete_book_from_favorites_existing_favorite_book_book_deleted(self, collector):
         collector.add_new_book('Ведьмак')
         collector.add_book_in_favorites('Ведьмак')
         collector.delete_book_from_favorites('Ведьмак')
+
         assert 'Ведьмак' not in collector.get_list_of_favorites_books()
 
-    def test_get_list_of_favorites_books_shows_books(self, collector):
+    def test_get_list_of_favorites_books_returns_favorites_books(self, collector):
         collector.add_new_book('Ведьмак')
         collector.add_book_in_favorites('Ведьмак')
+
         assert collector.get_list_of_favorites_books() == ['Ведьмак']
 
-
-    def test_add_book_in_favorites_cannot_be_twice(self, collector):
+    def test_add_book_in_favorites_same_book_twice_book_added_once(self, collector):
         collector.add_new_book('Ведьмак')
         collector.add_book_in_favorites('Ведьмак')
+        collector.add_book_in_favorites('Ведьмак')
+
+        assert collector.get_list_of_favorites_books() == ['Ведьмак']
         collector.add_book_in_favorites('Ведьмак')
         assert len(collector.get_list_of_favorites_books()) == 1
